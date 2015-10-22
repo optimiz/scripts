ddc3af3e0d47b062e0820c83da277eae25481cf1
Fabian Echevarria <fe2014@optimizing.net>
2015-10-22 14:46:38 -0700

    Refactored original script.
    Ignore empty desktops.  Send terminal window to background prior to snapshot. Rename variables. Tile screenshots into a collage.

--- screenshot_virtual_desktops.sh
+++ screenshot_virtual_desktops.sh
@@ -5,47 +5,59 @@
 #  To Public License, Version 2, as published by Sam Hocevar. See
 #  http://sam.zoy.org/wtfpl/COPYING for more details.
 
+function toggle_window {
+	for each in $terminals; do
+		wmctrl -r $each -i -b toggle,below;
+	done
+}
+
 # Define destination file name
 dest=$1
 if [ -z "$dest" ]; then
-    base=scrot-`date +%Y%m%d%H%M`
+    base=screenshot-$(date +%Y%m%d%H%M)
     ext=png
     dest=$base.$ext
 else
-    base=`basename $dest | cut -d. -f1`
-    ext=`echo $dest | cut -d. -f2-`
+    base=$(basename $dest | cut -d. -f1)
+    ext=$(echo -n $dest | cut -d. -f2)
 fi
 
+terminals=$(wmctrl -lp |grep $(ps -u $(whoami) |grep mate-terminal |cut -d' ' -f1) | cut -c-10)
+# Only count workspaces with active windows, not empty desktops.
+desktops=$(wmctrl -l | cut -c13 | sort -u | wc -l)
+# Send terminal window(s) behind others.
+toggle_window
+
+if [ "$desktops" -eq "1" ]; then
+	scrot $dest;
+	toggle_window;
+	exit 0;
+else
+# Make temp dir, clear contents on exit.
+	tmpdir=$(mktemp -d) || exit 1
+	trap 'rm -rf "$tmpdir"' EXIT INT TERM HUP
+	workspaces=$(wmctrl -l | cut -c13 | sort -u)
+	# Save current workspace
+	current=$(wmctrl -d | grep '*' | cut -d' ' -f1)
+	# Iterate over workspaces
+	for each in $workspaces; do
+		# Switch to workspace
+		wmctrl -s $each
+		# Pause for screen layout (xcompmgr/compiz animations) to complete
+		sleep 1.25
+		# Take workspace screenshot
+		# (Note: Prefer scrot; import ignores some transparency)
+		tmpdest=$tmpdir/$base-ws$each.$ext
+		scrot $tmpdest || import -window root $tmpdest
+	done
+
+# Return to original workspace and bring terminal window(s) back.
+	wmctrl -s $current
+	toggle_window
+
+# Tile as square/rectangular collage; use graphicsmagick instead of imagemagick; output as lossless JPEG 2000.
+	gm montage -geometry +0+0 -tile $(($desktops/2+$desktops%2))x$(($desktops/2+$desktops%2)) $tmpdir/$base-ws*.$ext -compress lossless $base.jp2
+
 # Remove tmp files
-rm -fr /tmp/$base-desk*.$ext
-
-# Save workspace count
-deskn=`wmctrl -d | wc -l`
-# Save workspace list
-desks=`wmctrl -d | cut -d' ' -f1`
-# Save current workspace
-current=`wmctrl -d | grep '*' | cut -d' ' -f1`
-
-# Iterate over workspaces
-for desk in $desks; do
-    # Move to such workspace
-    wmctrl -s $desk
-    # Take a rest (for xcompmgr/compiz animations)
-    sleep 3
-    # Take workspace screenshot
-    # (Note: I prefer scrot because import ignores some transparency)
-    tmpdest=/tmp/$base-desk$desk.$ext
-    scrot $tmpdest || import -window root $tmpdest
-done
-
-# Return to last workspace
-wmctrl -s $current
-
-# Concatenate all workspace screenshots
-montage -geometry +0+0 -tile 1x$deskn /tmp/$base-desk*.$ext $dest
-
-# Create thumbnail
-convert $dest -resize 256x`expr 256 \* $deskn` `echo $dest | cut -d. -f1`.thumb.$ext
-
-# Open screenshot
-thunar $dest || xdg-open $dest || gnome-open $dest || gpicview $dest || feh $dest || true >/dev/null 2>/dev/null
+	rm -rf "$tmpdir"
+fi
