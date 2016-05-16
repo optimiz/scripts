70e3b992ea04233f5e791b08756b202e8b679189
Fabian Echevarria <fe2014@optimizing.net>
2016-05-16 13:30:13 -0700

    reload-zones first, restart as fallback

--- pdns-blocklist-hosts.sh
+++ pdns-blocklist-hosts.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Monday, April 11 2016 - Import Mozilla Focus disconnect.me blocklist into PDNS-recursor v3.1+ (Fedora 20-23 use v3.7) via hosts file.
-# Idea (using lua) from https://blog.powerdns.com/2016/01/19/efficient-optional-filtering-of-domains-in-recursor-4-0-0/
-# Set "etc-hosts-file=/etc/pdns-recursor/blocklist.hosts" and "export-etc-hosts=on" in /etc/pdns-recursor/recursor.conf before restart.
+# Original idea (using lua) from https://blog.powerdns.com/2016/01/19/efficient-optional-filtering-of-domains-in-recursor-4-0-0/
+# Be sure to set "etc-hosts-file=/etc/pdns-recursor/blocklist.hosts" and "export-etc-hosts=on" in /etc/pdns-recursor/recursor.conf
 
 tmpdir=$(mktemp -d) || exit 1
 trap 'rm -rf "$tmpdir"' EXIT INT TERM HUP
@@ -13,11 +13,15 @@ https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt \
 https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt \
 https://s3.amazonaws.com/lists.disconnect.me/simple_malware.txt
 
-# Remove headers, duplicates and blank lines...
+# Skip headers, remove duplicates and blank lines...
 tail -n +5 simple*.txt |sort -u |grep -v ^$ > bl.txt
 
-# Create blocklist as hosts file...
+# Create hosts file from blocklist...
 while read each; do echo 127.0.0.1 $each ; done < bl.txt > /etc/pdns-recursor/blocklist.hosts
 
 popd
-systemctl restart pdns-recursor
+
+# NOTE: If "rec-control reload-zones" fails, restart instead -- the following error seems be a londstanding issue (since 2008)...
+# Error dealing with control socket request: Unable to send message over control channel '/var/run//lsock9CKhnj': No such file or directory
+
+rec-control reload-zones || systemctl restart pdns-recursor
