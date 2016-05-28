a66ea5162f1a06db49245d4a9997f2b5edc74e50
Fabian Echevarria <fe2014@optimizing.net>
2016-05-27 21:33:16 -0700

    changes for cron.daily
    Add quiet option and exit-on-error to wget command line. Add backup -- xz compresses backup file ~76% on average; runs via cron.daily.

--- pdns-blocklist-hosts.sh
+++ pdns-blocklist-hosts.sh
@@ -8,10 +8,12 @@ trap 'rm -rf "$tmpdir"' EXIT INT TERM HUP
 pushd "$tmpdir"
 
 # Retrieve lists from Mozilla Focus (disconnect.me); https://disconnect.me/trackerprotection/blocked; https://disconnect.me/trackerprotection/unblocked
-wget -N \
+wget -qN \
 https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt \
 https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt \
-https://s3.amazonaws.com/lists.disconnect.me/simple_malware.txt
+https://s3.amazonaws.com/lists.disconnect.me/simple_malware.txt || exit 1
+
+xz -c /etc/pdns-recursor/blocklist.hosts > /var/log/$(date +%Y%m%d)-blocklist.hosts.xz
 
 # Skip headers, remove duplicates and blank lines...
 tail -n +5 simple*.txt |sort -u |grep -v ^$ > bl.txt
@@ -21,7 +23,7 @@ while read each; do echo 127.0.0.1 $each ; done < bl.txt > /etc/pdns-recursor/bl
 
 popd
 
-# If "reload-zones" fails, restart instead.  The following error seems be a londstanding issue (2008?), increasing timeout resolves.
+# If "reload-zones" fails, restart instead.  The following error seems be a longstanding issue (2008?), increasing timeout resolves.
 # Error dealing with control socket request: Unable to send message over control channel '/var/run//lsock9CKhnj': No such file or directory
 
 rec_control --timeout=60 reload-zones || systemctl restart pdns-recursor
