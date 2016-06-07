06f260456525a3de727262986963e042f8a7ba87
Fabian Echevarria <fe2014@optimizing.net>
2016-06-06 17:30:03 -0700

    Add vob

--- old2mkv.sh
+++ old2mkv.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 # Script name: old2mkv.sh
-# Purpose: Convert outdated anime containers to MKV format, combining external subtitles.
+# Purpose: Convert outdated containers to MKV format, combining external subtitles.
 ionice -c3 -p$$
 
-for file in *.{ogm,divx};
+for file in *.{ogm,divx,vob};
 do
 	if [ -e "$file" -a ! -e "${file[@]%.*}.mkv" ]; then
 		basefile="${file[@]%.*}";
 		case ${file##*.} in
 			ogm )
-			mkvmerge -o "${basefile}.mkv" --language 1:jpn --language 2:eng "$file" --language 0:eng  "${basefile}.ssa" \
-			&& \
-			touch -r "$file" "${basefile}.mkv";
+			mkvmerge -o "${basefile}.mkv" --default-language en --language 1:ja "$file" "${basefile}.ssa" 
 			;; 
 			divx )
-			mkvmerge -o "${basefile}.mkv" --language 1:jpn "$file" \
-			&& \
-			touch -r "$file" "${basefile}.mkv";
-			;; 
+			mkvmerge -o "${basefile}.mkv" --default-language en --language 1:ja "$file" 
+			;;
+			vob )
+			mkvmerge -o "${basefile}.mkv" --default-language en "$file" 
+			;;
 			* )
 			echo 'Nothing to do.';
 			;;
 		esac;
+		touch -r "$file" "${basefile}.mkv";
 	fi;
 done
 #exit
 
 # Following revised from here: http://www.commandlinefu.com/commands/tagged/1978/cksfv
 
-for file in *.mkv; do mv "$file" "${file[@]%.*} [$(cksfv -b -q "$file" | egrep -o "\b[A-F0-9]{8}\b$")].${file##*.}"; done
+# for file in *.mkv; do mv "$file" "${file[@]%.*} [$(cksfv -b -q "$file" | egrep -o "\b[A-F0-9]{8}\b$")].${file##*.}"; done
