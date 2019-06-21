2263b050de6b18e534b75dea0cc828c7e703c539
Fabian E <fe2014@optimizing.net>
2019-06-21 16:51:16 -0700

    Create powerschool-sql-to-active-directory.ps1
    Create student accounts in AD directly from PowerSchool SQL extract via PowerShell.

---
+++ powerschool-sql-to-active-directory.ps1
@@ -0,0 +1,53 @@
+﻿# 2019-06-21 - FE - Create student accounts in AD directly from PowerSchool SQL extract.
+
+Import-Module SimplySql
+
+# 2019-06-14 - FE - Variabilize SQL connection criteria and other useful data.
+
+$dbpswd = '***********'
+$dbuser = 'PSNavigator'
+$dbport = 1521
+$dbname = 'PSPRODDB'
+$dbsource = 'Powerschool'
+
+$LogonWorkstations = 'class,instant,adfs,azure'
+$memberOf = 'CN=All Students,OU=Security,DC=example,DC=org' 
+$now = Get-Date -Format 'yyyy-MM-dd-HH.mm.ss.fff'
+$ExportPath = "$($env:USERPROFILE)\Desktop\$now-export.csv"
+
+Open-OracleConnection -ServiceName $dbname -DataSource $dbsource -Port $dbport -UserName $dbuser -Password $dbpswd -WarningAction SilentlyContinue
+
+$students=Invoke-SqlQuery -query "
+SELECT first_name, last_name, TRIM(CONCAT(dbms_random.STRING('a',3),dbms_random.STRING('x',5))) PASSWORD, student_web_id, psguid, CASE classof WHEN 0 THEN 9999 ELSE classof END CLASS 
+FROM students 
+WHERE (classof>='2020' OR (classof='0' AND student_number >= 100000)) AND student_web_id IS NOT NULL AND psguid IS NOT NULL AND exitcode IS NULL ORDER BY classof, last_name, first_name"
+
+# 2019-06-21 - FE - Original array splatting method from https://4sysops.com/archives/sync-active-directory-users-with-a-sql-database/
+
+foreach ($line in $students) {
+    ## Use PowerSchool web ID as username
+    $proposedUsername = $line.student_web_id
+    ## Check to see if exists in AD and print notice when true.
+    if (Get-AdUser -Filter "sAMAccountName -eq '$($proposedUsername)'") {
+ #       "The AD user [$proposedUsername] already exists."
+    } else { "ATTEMPTING TO CREATE [$proposedUsername]."
+        ## Otherwise, populate parameters using data from SQL extract, then splat into New-AdUser.
+        $newUserParams = @{
+            Name        = $proposedUsername
+            DisplayName = $line.first_name +' '+ $line.last_name
+            Path        = $("OU=$($line.class),OU=Students,OU=Classrooms,DC=example,DC=org")
+            Enabled     = $true
+            GivenName   = $line.first_name
+            Surname     = $line.last_name
+            EmployeeNumber  = $line.psguid
+            AccountPassword = ConvertTo-SecureString -String $line.PASSWORD -AsPlainText –Force
+            LogonWorkstations = $LogonWorkstations
+            UserPrincipalName = $proposedUsername
+            SamAccountName = $proposedUsername
+        }
+    ## Capture passwords to CSV for later mailmerge.
+        $line | Export-Csv -Path $ExportPath -NoTypeInformation -Append
+        New-AdUser @newUserParams -WhatIf
+        #Add-ADGroupMember -Identity $memberOf -Members $($proposedUsername) -WhatIf
+    }
+}
