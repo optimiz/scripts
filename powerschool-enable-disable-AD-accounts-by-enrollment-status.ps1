40facd6566d64a6decaaac20741ffbc36c436640
Fabian E <fe2014@optimizing.net>
2019-07-26 13:44:26 -0700

    Rewrite foreach for notification
    Rewrite foreach clauses to gather and then email results.

--- powerschool-enable-disable-AD-accounts-by-enrollment-status.ps1
+++ powerschool-enable-disable-AD-accounts-by-enrollment-status.ps1
@@ -17,16 +17,26 @@ $exitedstudents=Invoke-SqlQuery -query "SELECT student_web_id FROM students WHER
 $activestudents=Invoke-SqlQuery -query "SELECT student_web_id FROM students WHERE enroll_status <= 0 AND entrydate >= sysdate AND schoolid in (1234567,0) AND student_web_id IS NOT NULL AND psguid IS NOT NULL"
 
 # 2019-06-14 - JB - Add enabled check and pipe to disable command.
+# 2019-07-26 - JB - Set error action preference to ignore warning for student acccounts that have been completely deleted from AD.
+$ErrorActionPreference = "SilentlyContinue"
 
-foreach ($user in $exitedstudents)
-{
-    Get-ADUser -Identity $($user.STUDENT_WEB_ID) | ? { $_.ENABLED -eq "True" } | Disable-ADAccount #-WhatIf
+# 2019-07-26 - FE - Rewrite foreach so that notification of changes can be emailed.
+$temp1 = $exitedstudents | ForEach-Object { Get-ADUser -Identity $($_.STUDENT_WEB_ID) | ? { $_.ENABLED -eq "True" } }
+$temp1 | Disable-ADAccount #-WhatIf
+$temp2 = $activestudents | ForEach-Object { Get-ADUser -Identity $($_.STUDENT_WEB_ID) | ? { $_.ENABLED -ne "True" } }
+$temp2 | Enable-ADAccount #-WhatIf
+
+# 2019-07-26 - FE - Combine results into variable then email notification.
+$body = $temp1 |Select-Object enabled,samaccountname,name
+$body += $temp2 |Select-Object enabled,samaccountname,name
+
+$email = @{
+    From = "cronjob@example.org"
+    To = "it@example.org"
+    Subject = "Student Accounts Enabled/Disabled"
+    SMTPServer = "mx.example.org"
 }
 
-foreach ($user in $activestudents)
-{
-    Get-ADUser -Identity $($user.STUDENT_WEB_ID) | ? { $_.ENABLED -ne "True" } | Enable-ADAccount #-WhatIf
-}
-
+Send-MailMessage @email -Body ($body | Out-String)
 # 2019-06-21 - FE Actually delete inactive AD student accounts:
 # Get-ADUser -Filter * -SearchBase "OU=Students,OU=Classrooms,DC=example,DC=org" -Property Enabled | Where-Object {$_.Enabled -like “false”} | Remove-ADUser -WhatIf #| FT Name, Enabled -Autosize
