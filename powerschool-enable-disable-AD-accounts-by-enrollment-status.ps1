9d650bfa6f21d7444e2d52ac7d8e62bb0e55372e
Fabian E <fe2014@optimizing.net>
2019-07-31 14:59:58 -0700

    Create notification content conditionally
    Create email body only when actual data is processed.  Clear "body" variable on null exit so subsequent development doesn't email previous variable content now that notificaiton variable is created conditionally.  Added classof greater than or equal to current year to limit select; saves 8.2 milliseconds.

--- powerschool-enable-disable-AD-accounts-by-enrollment-status.ps1
+++ powerschool-enable-disable-AD-accounts-by-enrollment-status.ps1
@@ -14,7 +14,7 @@ $dbsource = 'Powerschool'
 Open-OracleConnection -ServiceName $dbname -DataSource $dbsource -Port $dbport -UserName $dbuser -Password $dbpswd -WarningAction SilentlyContinue
 
 $exitedstudents=Invoke-SqlQuery -query "SELECT student_web_id FROM students WHERE exitdate BETWEEN sysdate - 90 AND sysdate AND exitcode IS NOT NULL AND schoolid in (1234567,0) AND student_web_id IS NOT NULL AND psguid IS NOT NULL"
-$activestudents=Invoke-SqlQuery -query "SELECT student_web_id FROM students WHERE enroll_status <= 0 AND entrydate >= sysdate AND schoolid in (1234567,0) AND student_web_id IS NOT NULL AND psguid IS NOT NULL"
+$activestudents=Invoke-SqlQuery -query "SELECT student_web_id FROM students WHERE classof>=EXTRACT(YEAR FROM sysdate) and enroll_status <= 0 AND entrydate >= sysdate AND schoolid in (6938047,0) AND student_web_id IS NOT NULL AND psguid IS NOT NULL"
 
 # 2019-06-14 - JB - Add enabled check and pipe to disable command.
 # 2019-07-26 - JB - Set error action preference to ignore warning for student acccounts that have been completely deleted from AD.
@@ -22,16 +22,14 @@ $ErrorActionPreference = "SilentlyContinue"
 
 # 2019-07-26 - FE - Rewrite foreach so that notification of changes can be emailed.
 # 2019-07-29 - FE - Add conditionals to cmdlet processes; especially, do not send notification email if no changes were performed.
+# 2019-07-31 - FE - Reselect student account to show status AFTER change performed, not from before.
 
 $temp1 = $exitedstudents | ForEach-Object { Get-ADUser -Identity $($_.STUDENT_WEB_ID) | ? { $_.ENABLED -eq "True" } }
-if ($temp1) {$temp1 | Disable-ADAccount } else {'No accounts to disable.'}
+if ($temp1) {$temp1 | Disable-ADAccount; $body = $temp1 |Get-ADUser |Select-Object enabled,samaccountname,name } else {'No accounts to disable.'}
 $temp2 = $activestudents | ForEach-Object { Get-ADUser -Identity $($_.STUDENT_WEB_ID) | ? { $_.ENABLED -ne "True" } }
-if ($temp2) {$temp2 | Enable-ADAccount } else {'No accounts to reactivate.'}
+if ($temp2) {$temp2 | Enable-ADAccount; $body += $temp2 |Get-ADUser |Select-Object enabled,samaccountname,name } else {'No accounts to reactivate.'}
 
 # 2019-07-26 - FE - Combine results into variable then email notification.
-# 2019-07-31 - FE - Reselect student account to show status AFTER change performed, not from before.
-$body = $temp1 |Get-ADUser |Select-Object enabled,samaccountname,name
-$body += $temp2 |Get-ADUser |Select-Object enabled,samaccountname,name
 
 if ($body) {
     $email = @{
@@ -41,7 +39,7 @@ if ($body) {
         SMTPServer = "mx.example.org"
     }
     Send-MailMessage @email -Body ($body | Out-String)
-} else {return}
+} else {Clear-variable -Name 'body' ; return}
 
 # 2019-06-21 - FE Actually delete inactive AD student accounts:
 # Get-ADUser -Filter * -SearchBase "OU=Students,OU=Classrooms,DC=example,DC=org" -Property Enabled | Where-Object {$_.Enabled -like “false”} | Remove-ADUser -WhatIf #| FT Name, Enabled -Autosize
