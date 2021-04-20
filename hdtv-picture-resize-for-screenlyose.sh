0dcfb453c5883c3855a249aeab31408ea4d73fd9
Fabian E <fe2014@optimizing.net>
2021-04-20 09:02:56 -0700

    Rewrite for new system.
    New method rewrite.

--- hdtv-picture-resize-for-screenlyose.sh
+++ hdtv-picture-resize-for-screenlyose.sh
@@ -1,26 +1,37 @@
-#! /bin/bash
+#!/bin/bash
 # Monday, May 20 2019 - FE - Change exec of date command to $() style instead of legacy backticks and add folder variable for ease of use.
-# Currently runs via administrator crontab
-
 today=$(date '+%Y_%m_%d');
-destination='/home/screenlyOSE/displays'
+staging='/media/screenlymediafiles/staging';
+destination='/media/cbmscreenly';
+source='/media/photos/For_Displays';
 hdtv='1920x1080';
 
 # Tuesday, May 21 2019 - FE - Resize original pictures to HDTV size per screenly recommendations, read them here:
 # https://support.screenly.io/hc/en-us/articles/360009335693-What-content-types-are-supported-by-Screenly-OSE-
 # Monday, June 03 2019 - FE - Creator using different naming convention, updated to include change, and created 'outfile' variable for detox afterward.
-# Thursday, November 07 2019 - FE - Capture all different jpg/JPG names at the expense of ~.005 ms CPU/IO spent rechecking 'outfile' on already-resized files every run.
-# Friday, November 08 2019 - FE - Replace detox with exiftool to rename by date instead, that way resized files are a completely separate naming convention.
-# Tuesday, December 10 2019 - FE - Use touch to make new file name more useful.
 
-cd "$destination/"
-for infile in DSC*.{jpg,JPG};
-	do
-	if [ -e "${infile}" ] ; then
-		outfile="${infile[@]%.*}_resized_for_1080p_hdtv_screen.jpg"
-		convert "${infile}" -normalize -resize "$hdtv^" -gravity center -extent "$hdtv" +profile '*' -sampling-factor 4:2:0 -quality 89 "${outfile}";
-		touch -r "${infile}" "${outfile}";
-		mv "${infile}" "$destination/originals/";
-		exiftool -P '-filename<CreateDate' '-filename<filemodifydate' -d %Y-%m-%d-%Hh%Mm%S%%-c.%%le "${outfile}";
-	fi;
-	done;
+if [ -d "$source" ]; then
+	mkdir -p "$staging";
+	rsync -hurt --delete --exclude='Thumbs.db' "$source"/* "$staging/";
+
+	pushd "$staging"
+		for infile in *.{jpg,JPG,jpeg,JPEG};
+			do
+				if [ -e "${infile}" ] ; then
+					outfile="${infile[@]%.*}_resized_for_1080p_hdtv_screen.jpg";
+					convert "${infile}" -normalize -resize "$hdtv^" -gravity center -extent "$hdtv" +profile '*' -sampling-factor 4:2:0 -quality 89 "${outfile}";
+					jpegoptim "${outfile}";
+					touch -r "${infile}" "${outfile}";
+					exiftool -P '-filename<CreateDate' '-filename<filemodifydate' -d %Y-%m-%d-%Hh%Mm%S%%-c.%%le "${outfile}";
+					rm "${infile}";
+				fi;
+			done;
+	popd
+
+	rsync -hurt --delete "$staging/" '/media/screenlymediafiles/constant/' "$destination";
+fi
+
+# Friday, April 16 2021 - FE - Cleanup staging area.
+if [ -d "$staging" ]; then rm -rf "$staging"; fi
+
+exit;
