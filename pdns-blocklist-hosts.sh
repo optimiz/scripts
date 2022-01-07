39c7bfbf0cb1ecac77a9c00f527b5a310cd59826
Fabian E <fe2014@optimizing.net>
2022-01-06 18:44:10 -0800

    Parse from Github instead of AWS

--- pdns-blocklist-hosts.sh
+++ pdns-blocklist-hosts.sh
@@ -15,16 +15,10 @@ tmpdir=$(mktemp -d) || exit 1
 trap 'rm -rf "$tmpdir"' EXIT INT TERM HUP
 pushd "$tmpdir" || cd "$tmpdir"
 
-# Retrieve lists from Mozilla Focus (disconnect.me); https://disconnect.me/trackerprotection/blocked; https://disconnect.me/trackerprotection/unblocked
-# Friday, February 03 2017 - Test new list from https://www.malwaredomains.com/
-wget -qN --header="Accept-Encoding: gzip" \
-https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt \
-https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt \
-https://s3.amazonaws.com/lists.disconnect.me/simple_malware.txt \
-https://mirror1.malwaredomains.com/files/justdomains || exit 1
-
-# Skip headers, remove duplicates and blank lines...
-tail -n +5 simple*.txt |sort -ifu |grep -v ^$ > 'disconnectme'
+# Monday, December 20 2021 - AWS Disconnect.me downloads no longer updating as of mid-2020, download and parse raw JSON direct from github instead…
+curl -sOA "$agent" --compressed  'https://raw.githubusercontent.com/disconnectme/disconnect-tracking-protection/master/services.json'
+for each in $(jq -r '.categories| keys| .[]' 'services.json') ;do jq -r ".categories.$each[]|.[]|.[]|.[0]" 'services.json' |grep -v '^jq: error' > "simple_$each.txt" ; done
+sort -ifu simple_{Advertising,Analytics,Cryptomining,Disconnect,FingerprintingGeneral,FingerprintingInvasive,Social}.txt > 'disconnectme'
 
 # Need additional lists because Firefox Electrolysis (e10s) disables userContent.css see: https://bugzilla.mozilla.org/show_bug.cgi?id=1046166
 curl -sA "$agent" --compressed 'https://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts' -o 'yoyo'
@@ -33,15 +27,17 @@ curl -sA "$agent" --compressed 'https://www.malwaredomainlist.com/hostslist/host
 # Create hosts file from blocklist...
 if [ -s 'disconnectme' ]; then while read each; do echo 127.0.0.1 $each; done < 'disconnectme' > "$pdnslocation/disconnect.hosts"; fi
 if [ -s 'yoyo' ]; then grep 127.0.0.1 'yoyo' > '/etc/pdns-recursor/yoyo.hosts'; fi
-if [ -s 'malwaredomainlist' ]; then grep -v localhost 'malwaredomainlist' |grep 127.0.0.1 > "$pdnslocation/mdl.hosts"; fi
 
 popd || cd -
+
+# Tuesday, June 16 2020 - Conform downloads to unix style line endings.
+dos2unix /etc/pdns-recursor/{intranet,manual,disconnect,yoyo,nocoin}.hosts
 
 # Tuesday, August 30 2016 - Add manual, intranet and adserver lists; combine into single input -- PDNS won't accept multiple "etc-hosts-file".
 # Saturday, September 03 2016 - Normalize whitespace so sort can eliminate more duplicates.
 # sort -ifu /etc/pdns-recursor/{intranet,manual,disconnect,yoyo,mdl}.hosts -o /etc/pdns-recursor/pdns.hosts
 # Friday, May 18 2018 - Add sed to change 0.0.0.0 to 127.0.0.1, on my systems, 127.0.0.1 is faster than 0.0.0.0
-grep -Ehv '(127.0.0.1$|==|::|^#|^$)' $pdnslocation/{intranet,manual,disconnect,yoyo,mdl,nocoin}.hosts |sed 's/0.0.0.0/127.0.0.1/g' |tr [:upper:] [:lower:] |tr -s [:blank:] |sort -ifu -o "$pdnslocation/pdns.hosts"
+grep -Ehv '(127.0.0.1$|==|::|^#|^$)' $pdnslocation/{intranet,manual,disconnect,yoyo,nocoin}.hosts |sed 's/0.0.0.0/127.0.0.1/g' |tr [:upper:] [:lower:] |tr -s [:blank:] |grep -vf "$pdnslocation/whitelist.hosts" |sort -ifu -o "$pdnslocation>
 
 # If "reload-zones" fails, restart instead.  The following error seems be a longstanding issue (2008?), increasing timeout resolves.
 # Error dealing with control socket request: Unable to send message over control channel '/var/run//lsock9CKhnj': No such file or directory
