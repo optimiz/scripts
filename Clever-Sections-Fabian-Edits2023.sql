594493e4531b04f0dffe642fd04238c9df6af931
Fabian E <fe2014@optimizing.net>
2024-02-22 14:52:52 -0800

    all 2024 updates
    Now selects all co-teacher categories.  Add long term substitutes now that all co-teacher categories are included.  Limit teachers to those with current active sections, and sections to those with enrolled students.

--- Clever-Sections-Fabian-Edits2023.sql
+++ Clever-Sections-Fabian-Edits2023.sql
@@ -3,7 +3,15 @@
 ** If multiple teacher categories are assigned, the highest employee ID is selected.*/
 
 SELECT school_id, section_id, MAX(teacher) AS teacher_id, MAX(coteacher) AS teacher_2_id, 
---MAX(jobshareteacher) AS teacher_3_id, MAX(teacheraide) AS teacher_4_id, MAX(classobserver) AS teacher_5_id, MAX(studentteacher) AS teacher_6_id,
+MAX(jobshareteacher) AS teacher_3_id, MAX(teacheraide) AS teacher_4_id, 
+MAX(classobserver) AS teacher_5_id, MAX(studentteacher) AS teacher_6_id,
+
+/* Friday, January 26 2024 - FE - Add MIN clauses to capture additional co-teachers. */
+CASE WHEN MIN(coteacher) <> MAX(coteacher) THEN MIN(coteacher) ELSE NULL END AS teacher_7_id, 
+CASE WHEN MIN(jobshareteacher) <> MAX(jobshareteacher) THEN MIN(jobshareteacher) ELSE NULL END AS teacher_8_id, 
+CASE WHEN MIN(teacheraide) <> MAX(teacheraide) THEN MIN(teacheraide) ELSE NULL END AS teacher_9_id, 
+CASE WHEN MIN(studentteacher) <> MAX(studentteacher) THEN MIN(studentteacher) ELSE NULL END AS teacher_10_id,
+
 NAME, section_number, grade, course_name, course_number, course_description, PERIOD, subject, term_name, term_start, term_end
 FROM
 (
@@ -11,15 +19,14 @@ SELECT DISTINCT
     to_char(sec.schoolid) school_id,
     to_char(sec.ID) section_id,
     CASE WHEN to_char(sctc.roleid) = '25' THEN to_char(te.teachernumber) END AS teacher,
-    CASE WHEN to_char(sctc.roleid) <> '25' THEN to_char(te.teachernumber) END AS coteacher,
-/*
+/* Wednesday, January 10 2024 - FE - SIS team wants all defined co-teacher categories included.*/
     CASE WHEN to_char(sctc.roleid) = '26' THEN to_char(te.teachernumber) END AS coteacher,
     CASE WHEN to_char(sctc.roleid) = '27' THEN to_char(te.teachernumber) END AS jobshareteacher,
     CASE WHEN to_char(sctc.roleid) = '28' THEN to_char(te.teachernumber) END AS teacheraide,
     CASE WHEN to_char(sctc.roleid) = '29' THEN to_char(te.teachernumber) END AS classobserver,
     CASE WHEN to_char(sctc.roleid) = '41' THEN to_char(te.teachernumber) END AS studentteacher,
-*/
-    /*NULL AS teacher_7_id,    NULL AS teacher_8_id,    NULL AS teacher_9_id,    NULL AS teacher_10_id,*/
+
+    NULL AS teacher_7_id,    NULL AS teacher_8_id,    NULL AS teacher_9_id,    NULL AS teacher_10_id,
     C.course_name || '-' || sec.section_number || '-' || sec.expression NAME,
     sec.section_number,
     CASE
@@ -41,12 +48,15 @@ FROM sections sec
     JOIN courses C ON sec.course_number=C.course_number
     JOIN cc ON sec.ID=ABS(cc.sectionid) 
     JOIN terms T ON sec.termid=T.ID AND sec.schoolid=T.schoolid 
-    JOIN sectionteacher sctc ON sctc.sectionid = sec.ID 
-    JOIN teachers te ON te.ID = sctc.teacherid AND te.status = 1 AND te.staffstatus = 1
+/* Monday, February 12 2024 - FE - Limit teacher selection to those currently active in a section. */
+    JOIN sectionteacher sctc ON sctc.sectionid = sec.ID AND sctc.end_date >= sysdate
+/* Thursday, January 25 2024 - FE - Add long term substitute (4) code. */
+    JOIN teachers te ON te.ID = sctc.teacherid AND te.status = 1 AND te.staffstatus in (1,4)
 WHERE 1=1
-    AND sec.schoolid IN (SELECT DISTINCT schoolid FROM terms WHERE lastday >= sysdate ) 
-    AND sec.schoolid NOT IN ('800','999','999999','950','951','955','981','982','983')
+    AND sec.schoolid IN (SELECT DISTINCT schoolid FROM terms WHERE lastday >= sysdate AND schoolid NOT IN ('800','999','999999','950','951','955','981','982','983'))
     AND sctc.start_date >= (SELECT DISTINCT MIN(firstday) FROM terms WHERE lastday >= sysdate )
-)
+/* Thursday, February 22 2024 - FE - Don't include sections that have no students.*/
+    AND sec.no_of_students > '0'
+)  
 GROUP BY school_id, section_id, NAME, section_number, grade, course_name, course_number, course_description, PERIOD, subject, term_name, term_start, term_end
 ORDER BY 1,2,3,4 ;
